f86134c60fab4aa9802fb1494619c934272407c7
Joshi <madugulajoshi011@gmail.com>
2024-04-24 18:46:44 +0530

    succesful connection to mongo-db

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+const app = express();
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started and listening to port ${process.env.PORT}`);
+});
